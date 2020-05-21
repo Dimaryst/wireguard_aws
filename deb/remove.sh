5f93257b9a4e53c0656ff38c3123d5262f944cff
Danil Yeremeyev <sorhajy@icloud.com>
2020-05-21 22:50:22 +0700

    ADD removing ppa repo

--- deb/remove.sh
+++ deb/remove.sh
@@ -8,6 +8,7 @@ systemctl disable wg-quick@wg0
 echo "# Removing"
 yes | apt autoremove wireguard wireguard-dkms wireguard-tools
 #yes | apt autoremove software-properties-common
+add-apt-repository --remove ppa:wireguard/wireguard
 yes | apt update
 
 echo "# Clearing folder"
